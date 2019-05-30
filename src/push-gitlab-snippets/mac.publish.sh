0693864423c3261def5a6c6f891fc0058d1e808b
Alan <alan.wei@live.com>
2019-05-30 21:48:16 +0800

    feat: change file executable mode

--- src/push-gitlab-snippets/mac.publish.sh
+++ src/push-gitlab-snippets/mac.publish.sh
@@ -4,5 +4,5 @@ rm -rf ../../publish/
 echo "publish..."
 dotnet script publish main.csx -o ../../publish -c Release -r osx-x64
 echo "packaging..."
-./macos-x64.warp-packer --arch macos-x64 --input_dir ../../publish --exec script --output ../../releases/dwz.mac
+../../tools/macos-x64.warp-packer --arch macos-x64 --input_dir ../../publish --exec script --output ../../releases/dwz.mac
 chmod +x ../../releases/dwz.mac
