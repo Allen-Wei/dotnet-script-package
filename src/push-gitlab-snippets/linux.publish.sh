161f59d280fd674f44cac03dbd6712b45a196e84
Alan <alan.wei@live.com>
2019-05-30 22:01:54 +0800

    refactor: Rename executable file name

--- src/push-gitlab-snippets/linux.publish.sh
+++ src/push-gitlab-snippets/linux.publish.sh
@@ -1,8 +1,8 @@
 echo "clean..."
-rm ../../releases/dwz.linux
+rm ../../releases/push-gitlab-snippets.linux
 rm -rf ../publish
 echo "publish..."
 dotnet script publish main.csx -o ../../publish -c Release -r linux-x64
 echo "packaging..."
-../../tools/linux-x64.warp-packer --arch linux-x64 --input_dir ../../publish --exec script --output ../../releases/dwz.linux
-chmod +x ../../releases/dwz.linux
+../../tools/linux-x64.warp-packer --arch linux-x64 --input_dir ../../publish --exec script --output ../../releases/push-gitlab-snippets.linux
+chmod +x ../../releases/push-gitlab-snippets.linux
