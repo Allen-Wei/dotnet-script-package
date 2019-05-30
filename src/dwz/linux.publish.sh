fb48c2ae4def98a46847c6fc43e6b871982c4714
Alan <alan.wei@live.com>
2019-05-30 21:38:14 +0800

    feat: 重命名release

--- src/dwz/linux.publish.sh
+++ src/dwz/linux.publish.sh
@@ -1,8 +1,8 @@
 echo "clean..."
-rm ../../release/dwz.linux
+rm ../../releases/dwz.linux
 rm -rf ../publish
 echo "publish..."
 dotnet script publish main.csx -o ../../publish -c Release -r linux-x64
 echo "packaging..."
-../../tools/linux-x64.warp-packer --arch linux-x64 --input_dir ../../publish --exec script --output ../../release/dwz.linux
-chmod +x ../../release/dwz.linux
+../../tools/linux-x64.warp-packer --arch linux-x64 --input_dir ../../publish --exec script --output ../../releases/dwz.linux
+chmod +x ../../releases/dwz.linux
--- src/dwz/mac.publish.sh
+++ src/dwz/mac.publish.sh
@@ -1,8 +1,8 @@
 echo "clean..."
-rm ../../release/dwz.mac
+rm ../../releases/dwz.mac
 rm -rf ../../publish/
 echo "publish..."
 dotnet script publish main.csx -o ../../publish -c Release -r osx-x64
 echo "packaging..."
-./macos-x64.warp-packer --arch macos-x64 --input_dir ../../publish --exec script --output ../../release/dwz.mac
-chmod +x ../../release/dwz.mac
+./macos-x64.warp-packer --arch macos-x64 --input_dir ../../publish --exec script --output ../../releases/dwz.mac
+chmod +x ../../releases/dwz.mac
