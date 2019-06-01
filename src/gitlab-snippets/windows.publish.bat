fb6db89345e60710ba739d243d2970d2f3d03068
Alan <alan.wei@live.com>
2019-06-01 14:19:47 +0800

    feat: optimize shell

--- src/gitlab-snippets/windows.publish.bat
+++ src/gitlab-snippets/windows.publish.bat
@@ -1,3 +1,3 @@
 rmdir /S /Q ..\..\publish
 dotnet script publish main.csx -o ..\..\publish -c Release -r win10-x64
-..\..\tools\windows-x64.warp-packer.exe --arch windows-x64 --input_dir ..\..\publish --exec script.exe --output ..\..\releases\push-gitlab-snippets.win.exe
+..\..\tools\windows-x64.warp-packer.exe --arch windows-x64 --input_dir ..\..\publish --exec script.exe --output ..\..\releases\gitlab-snippets.win.exe
